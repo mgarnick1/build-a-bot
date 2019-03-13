d6072c3c8a8254ddf6157fbd986d794b9f06d6c1
Mark <mgarnick@techtonicgroup.com>
2019-03-13 11:42:18 -0600

    setup homepage and navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png">
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  width: 1024px;
+  min-height: 300px;
+}
+
+.nav-item {
+}
+
+.logo {
 }
 </style>
