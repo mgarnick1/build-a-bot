81d8ea57ff16737bb57612ef29978b8c5e3ab4af
Mark <mgarnick@techtonicgroup.com>
2019-03-15 11:56:31 -0600

    validating props in a child component passed to parent component

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -19,7 +19,16 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  props: ["parts", "position"],
+  props: {
+    parts: { type: Array, required: true },
+    position: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        return ["left", "right", "center", "bottom", "top"].includes(value);
+      }
+    }
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
