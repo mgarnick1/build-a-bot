4be52b26049ba94de20c391ef982b97041066363
Mark <mgarnick@techtonicgroup.com>
2019-03-15 15:03:02 -0600

    add sidebars and add named views

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import RobotHeads from '../parts/RobotHeads.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
+import SideBarStandard from '../sidebars/SideBarStandard.vue';
+import SideBarBuild from '../sidebars/SideBarBuild.vue';
 
 Vue.use(Router);
 
@@ -16,12 +18,18 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: HomePage,
+      components: {
+        default: HomePage,
+        sidebar: SideBarStandard,
+      },
     },
     {
       path: '/build',
       name: 'Build',
-      component: RobotBuilder,
+      components: {
+        default: RobotBuilder,
+        sidebar: SideBarBuild,
+      },
     },
     {
       path: '/parts/browse',
