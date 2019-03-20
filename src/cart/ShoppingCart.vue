5604263ab90cd2c1d93c899334652283aa63fb70
Mark <mgarnick@techtonicgroup.com>
2019-03-20 09:56:00 -0600

    add getters to the store to access data.

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -15,6 +15,21 @@
         </tr>
       </tbody>
     </table>
+    <h2 class="saleItems">You saved money on these robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">Robot</th>
+          <th class="cost">Cost</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">{{robot.head.title}}</td>
+          <td class="cost">{{robot.cost}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -23,7 +38,10 @@ export default {
   name: "Cart",
   computed: {
     cart() {
-        return this.$store.state.cart;
+      return this.$store.state.cart;
+    },
+    cartSaleItems() {
+        return this.$store.getters.cartSaleItems;
     },
   },
 };
@@ -40,5 +58,10 @@ th {
 }
 .cost {
   text-align: right;
+}
+.saleItems {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
 }
 </style>
