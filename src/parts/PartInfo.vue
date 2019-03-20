343238aa7ceab42cf8123e3c84dd2001d0ac15da
Mark <mgarnick@techtonicgroup.com>
2019-03-20 13:35:46 -0600

    setup actions and get request to pull from the API

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -6,24 +6,25 @@
 </template>
 
 <script>
-import parts from '../data/parts';
+import getPartsMixin from "./get-parts-mixin";
 
 export default {
-  name: 'PartInfo',
+  name: "PartInfo",
+  mixins: [getPartsMixin],
   props: {
     partType: { type: String },
     id: {
       type: [Number, String],
       validator(value) {
         return Number.isInteger(Number(value));
-      },
-    },
+      }
+    }
   },
   computed: {
     part() {
       const { partType, id } = this;
-      return parts[partType].find(part => part.id === +id);
-    },
-  },
+      return this.parts[partType].find(part => part.id === +id);
+    }
+  }
 };
 </script>
