660b36209c348bd354cc1fd37a79d759519a69a2
Mark <mgarnick@techtonicgroup.com>
2019-03-20 14:52:40 -0600

    organizing store modules

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -95,7 +95,7 @@ export default {
   mixins: [createdHookMixin],
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     headBorderStyle() {
       return {
