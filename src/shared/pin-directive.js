707e48cf8f2d41a8771d7613344faee93bdf0ea5
Mark <mgarnick@techtonicgroup.com>
2019-03-21 14:19:13 -0600

    combine the bind and update modifiers by creating a functin that does both.

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,14 +1,7 @@
-function applyStyle(element, binding) {
+
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-export default {
-  bind: (element, binding) => {
-    applyStyle(element, binding);
-  },
-  update: (element, binding) => {
-    applyStyle(element, binding);
-  },
-};
