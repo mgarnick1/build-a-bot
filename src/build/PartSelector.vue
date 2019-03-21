f7fe5e0fda17054375a065552cbcf6d593e65a9a
Mark <mgarnick@techtonicgroup.com>
2019-03-21 14:14:16 -0600

    update bind directive to hot reload.

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="part" :class="position">
+    {{ pinPadding }}
     <router-link
       :to="{
       name: 'Parts',
@@ -12,7 +13,12 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin="{ bottom: '10px', right: '50px'}" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span
+      @click="pinPadding='30px'"
+      v-pin="{ bottom: pinPadding, right: pinPadding}"
+      class="sale"
+      v-show="selectedPart.onSale"
+    >Sale!</span>
   </div>
 </template>
 
@@ -41,7 +47,7 @@ export default {
     }
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, pinPadding: "10px" };
   },
   computed: {
     selectedPart() {
